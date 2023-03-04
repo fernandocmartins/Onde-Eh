75db665d2282a461c09a6a11f4063f6c60cdbaeb
Fernando <fernandocsrmartins@gmail.com>
2023-03-04 14:08:17 -0300

    initial commit, add imgs

---
+++ app/src/main/java/com/fernando/ondeeh/AddressDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.fernando.ondeeh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddressDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_detail)
+    }
+}
